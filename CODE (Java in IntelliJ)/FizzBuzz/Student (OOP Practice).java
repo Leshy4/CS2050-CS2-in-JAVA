ba82d1c2b459a50b656ca370f8ebfd3ac1f476f1
Leshy4 <48464154+Leshy4@users.noreply.github.com>
2020-10-26 00:10:25 -0600

    Add files via upload

---
+++ CODE (Java in IntelliJ)/FizzBuzz/Student (OOP Practice).java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Student implements Comparable<Student> {
+
+    private int    id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return "(" + id + ", '" + name + "')";
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return name.compareTo(other.name);
+    }
+}
